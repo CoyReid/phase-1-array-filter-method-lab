d0ed9608fabedee79a196017ae007ec294d8630f
Coy Reid <coyreid7@gmail.com>
2021-07-19 21:32:04 -0500

    commit tree 69e1865a2a28f3e4064c772dd04430fd235cb021

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(match => match.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(match => match.toLowerCase().indexOf(string.toLowerCase()) === 0);
+}
+
+function matchName(array, string) {
+    return array.filter(match => match.name === string);
+}
